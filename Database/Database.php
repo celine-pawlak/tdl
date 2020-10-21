236f55379c1d9961b30bcee56f43162192bb818d
Céline Pawlak <celine.pawlak@laplateforme.io>
2020-10-21 15:09:54 +0200

    Connexion à la BDD

--- Database/Database.php
+++ Database/Database.php
@@ -3,5 +3,27 @@
 
 class Database
 {
+    private $db_name;
+    private $db_user;
+    private $db_pass;
+    private $db_host;
+    private $pdo;
 
+    public function __construct($db_name, $db_user = 'root', $db_pass = '', $db_host = 'localhost')
+    {
+        $this->db_name = $db_name;
+        $this->db_user = $db_user;
+        $this->db_pass = $db_pass;
+        $this->db_host = $db_host;
+    }
+
+    public function getPDO()
+    {
+        if ($this->pdo === null){
+            $pdo = new PDO('mysql:dbname='.$this->db_name.';host='.$this->db_host.'', 'root', '');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo = $pdo;
+        }
+        return $this->pdo;
+    }
 }
---
+++ todolist.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do List</title>
+    <!--<link rel="icon" type="image/png" href="#" />  LOGO A AJOUTER  -->
+
+    <!--Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+
+    <!-- Materialize icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Materialize and CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="ressources/style.css">
+
+    <!-- Js & Jquery -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="ressources/script.js"></script>
+</head>
+<body>
+<header></header>
+<main id="main-to_do_list">
+NOICE
+</main>
+<footer></footer>
+</body>
+</html>
